119cab6bc132047bd80ea1e84c15a177a9c12f18
pretofusia1 <preto.acc@mailbox.org>
2025-09-30 22:47:28 +0200

    wabar fonts

--- modules/common.nix
+++ modules/common.nix
@@ -7,7 +7,7 @@
   fonts = {
     packages = with pkgs; [
       noto-fonts noto-fonts-cjk-sans noto-fonts-emoji
-      nerd-fonts.fira-code nerd-fonts.jetbrains-mono
+      nerd-fonts.fira-code nerd-fonts.jetbrains-mono nerd-fonts.symbols-only
     ];
     enableDefaultPackages = true;
   };
