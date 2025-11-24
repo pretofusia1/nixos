47273416d8753f5734695e1dc8f25c6f6b6c23d6
pretofusia1 <preto.acc@mailbox.org>
2025-11-24 15:00:25 +0100

    Merge conflict resolved: MAC randomization disabled

--- modules/security-advanced.nix
+++ modules/security-advanced.nix
@@ -75,24 +75,13 @@
   # -------------------------------------------
   # 4. MAC-Adress-Randomisierung (DEAKTIVIERT)
   # -------------------------------------------
-<<<<<<< HEAD
-  # Verhindert WiFi/Ethernet-Tracking
-#  networking.networkmanager = {
-#    wifi.macAddress = "random";
-#    ethernet.macAddress = "random";
-    # Randomisiere bei jedem Connect (nicht nur Boot)
-#    wifi.scanRandMacAddress = true;
-#  };
-=======
-  # HINWEIS: Kann DNS-Probleme verursachen wenn Router MAC-basiert konfiguriert ist
-  # Auskommentiert auf Wunsch des Users
-  # networking.networkmanager = {
-  #   wifi.macAddress = "random";
-  #   ethernet.macAddress = "random";
-  #   wifi.scanRandMacAddress = true;
-  # };
->>>>>>> refs/remotes/origin/main
-
+    # HINWEIS: Kann DNS-Probleme verursachen wenn Router MAC-basiert konfiguriert ist
+    # Auskommentiert auf Wunsch des Users
+    # networking.networkmanager = {
+    #   wifi.macAddress = "random";
+    #   ethernet.macAddress = "random";
+    #   wifi.scanRandMacAddress = true;
+    # };
   # -------------------------------------------
   # 5. Erweiterte Netzwerk-Härtung
   # -------------------------------------------
